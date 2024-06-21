3443017cb37f47d0c8573904593fe40659dc1543
aldomarcelino <aldo115marcelino@gmail.com>
2024-06-21 10:39:21 +0700

    add camera

--- app/(tabs)/camera.tsx
+++ app/(tabs)/camera.tsx
@@ -1,43 +1,42 @@
-import Ionicons from "@expo/vector-icons/Ionicons";
 import { StyleSheet, Image, Platform, Text, Linking } from "react-native";
 
-// import { Collapsible } from "@/components/Collapsible";
-// import { ExternalLink } from "@/components/ExternalLink";
-// import ParallaxScrollView from "@/components/ParallaxScrollView";
-// import { ThemedText } from "@/components/ThemedText";
-// import { ThemedView } from "@/components/ThemedView";
-// import {
-//   Camera,
-//   useCameraDevice,
-//   useCameraPermission,
-// } from "react-native-vision-camera";
-// import { useEffect } from "react";
+import { Collapsible } from "@/components/Collapsible";
+import { ExternalLink } from "@/components/ExternalLink";
+import ParallaxScrollView from "@/components/ParallaxScrollView";
+import { ThemedText } from "@/components/ThemedText";
+import { ThemedView } from "@/components/ThemedView";
+import {
+  Camera,
+  useCameraDevice,
+  useCameraPermission,
+} from "react-native-vision-camera";
+import { useEffect } from "react";
 
 export default function TabTwoScreen() {
-  // const device = useCameraDevice("front");
-  // const { hasPermission } = useCameraPermission();
+  const device = useCameraDevice("front");
+  const { hasPermission } = useCameraPermission();
 
-  // useEffect(() => {
-  //   const getPermision = async () => {
-  //     const permision = await Camera.requestCameraPermission();
-  //     console.log("camera permision status " + permision);
-  //     if (permision == "denied") await Linking.openSettings();
-  //     else {
-  //       const newCameraPermission = await Camera.requestCameraPermission();
-  //       const newMicrophonePermission =
-  //         await Camera.requestMicrophonePermission();
-  //       console.log("2 camera permision status " + permision);
-  //     }
-  //   };
-  //   getPermision();
-  // }, []);
+  useEffect(() => {
+    const getPermision = async () => {
+      const permision = await Camera.requestCameraPermission();
+      console.log("camera permision status " + permision);
+      if (permision == "denied") await Linking.openSettings();
+      else {
+        const newCameraPermission = await Camera.requestCameraPermission();
+        const newMicrophonePermission =
+          await Camera.requestMicrophonePermission();
+        console.log("2 camera permision status " + permision);
+      }
+    };
+    getPermision();
+  }, []);
 
-  // if (!hasPermission) return <Text>ikfasdjfsda jfasdan</Text>;
-  // if (device == null) return <Text>bebek gorengggg</Text>;
-  // <Camera style={StyleSheet.absoluteFill} device={device} isActive={true} />
+  if (!hasPermission) return <Text>ikfasdjfsda jfasdan</Text>;
+  if (device == null) return <Text>bebek gorengggg</Text>;
   return (
     <>
-      <Text>Maukkkfjasldjfasd jfasldjfsd</Text>
+      <Camera style={StyleSheet.absoluteFill} device={device} isActive={true} />
+      {/* <Text>Maukkkfjasldjfasd jfasldjfsd</Text> */}
     </>
   );
 }
